40026fe4e1bbc5d7d5f257b60635036c3907a424
ThanksXly <328704719@qq.com>
2019-11-30 20:32:12 +0800

    '随意'

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('koa-router')();
 const moment = require('moment');
-
+//
 const userSql = require('../allSqlStatement/userSql');
 
 // 随机生成用户名
@@ -8,7 +8,7 @@ function randomUsername() {
     let str = "",
     arr = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 
+    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
     'y', 'z','0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
   ];
     for (let i = 0; i < 8; i++) {
